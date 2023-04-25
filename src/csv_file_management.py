d18f8c6ac6cd65d356e86e1de1840e0e9cb714f1
Crista <crista.holmstrom@outlook.com>
2023-04-25 22:51:35 +0300

    Viimeiset päivitykset ennen deadlinea

--- src/csv_file_management.py
+++ src/csv_file_management.py
@@ -1,18 +1,23 @@
-
+from pathlib import Path
 
 class CSVfiles:
     def write_to_file(self, filename, day, amount, what):
-        name_of_file = filename
+        name_of_file = f"{filename}.csv"
         with open(name_of_file, "a") as file:
             file.write(f"{day};{amount};{what}\n")
         self.read_file(filename)
 
     def read_file(self, name_of_file):
+        filename = f"{name_of_file}.csv"
         dictionary = {}
-        if not name_of_file:
-            with open(name_of_file, "a") as file:
-                file.write("0;0;0\n")
-        with open(name_of_file, encoding="utf-8") as file:
+        filepath = Path(filename)
+        filepath.touch(exist_ok= True)
+        open(filepath,"a")
+
+        #if not filename:
+         #   with open(filename, "w") as file:
+          #      file.write("0;0;0\n")
+        with open(filename) as file:
             for row in file:
                 row = row.replace("\n", "")
                 section = row.split(";")
